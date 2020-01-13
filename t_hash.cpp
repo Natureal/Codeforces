2048b3693b6809afcd44bce23fff885a0bf0248a
Natureal <naturechenpeng@gmail.com>
2020-01-13 18:52:23 +0900

    Mon Jan 13 18:52:23 JST 2020

--- t_hash.cpp
+++ t_hash.cpp
@@ -1,11 +1,24 @@
-const int mod[2] = {1000000007, 1000000009};
-const int base[2] = {23, 29};
+// Define maxn. Base-B number system.
+const int maxn = ;
 
-int HS[2][1000010];
-
-void Pre_HS(int len){
-    HS[0][1] = HS[1][1] = 1;
-    REP(i, 2, len){
-        REP(j, 0, 1) HS[j][i] = (1LL * HS[j][i - 1] * base[j]) % mod[j];
+struct Hash{
+    // hash is 1-based index, but the input and output are 0-based index
+    int base[2][maxn], hs[2][maxn], len;
+    int B[2] = {239, 229}, mod[2] = {1000 * 1000 * 1000 + 7, 1000 * 1000 * 1000 + 9};
+    void init(char *s, int L = -1){
+        len = (L == -1) ? strlen(s) : L;
+        base[0][0] = base[1][0] = 1;
+        hs[0][0] = hs[1][0] = 0;
+        REP(i, 1, len) REP(j, 0, 1){
+            base[j][i] = 1LL * base[j][i - 1] * B[j] % mod[j];
+            hs[j][i] = (1LL * hs[j][i - 1] * B[j] % mod[j] + s[i - 1]) % mod[j];
+        }
     }
-}
+    pii get(int l, int r){ // 0-based index
+        int res[2];
+        REP(i, 0, 1)
+            res[i] = ((hs[i][r + 1] - 1LL * hs[i][l] * base[i][r - l + 1] % mod[i])
+                        % mod[i] + mod[i]) % mod[i];
+        return MP(res[0], res[1]);
+    }
+}HS;
